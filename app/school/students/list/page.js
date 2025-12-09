0a9de9ae19b1fa3abf4dec1e1a06644ec12380c8
Rajan Kumar <52025580+RajankrSingh@users.noreply.github.com>
2025-12-10 00:10:20 +0530

    build time error fix

--- app/school/students/list/page.js
+++ app/school/students/list/page.js
@@ -263,7 +263,7 @@ export default function ListStudentsPage() {
                 <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
-                <span>Change status to 'Ready to Print'</span>
+                <span>Change status to &apos;Ready to Print&apos;</span>
               </button>
             </div>
           )}
